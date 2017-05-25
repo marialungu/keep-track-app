b475f29c75e034310891b27a2a37936596468c98
Vlad Vartolomei <vlad.vartolomei@gmail.com>
2017-05-25 21:19:44 +0300

    spring initilizer spring boot 1.5.3

---
+++ src/main/java/com/keeptrack/rest/api/keeptrackapi/KeepTrackApiApplication.java
@@ -0,0 +1,12 @@
+package com.keeptrack.rest.api.keeptrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepTrackApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/keeptrack/rest/api/keeptrackapi/KeepTrackApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keeptrack.rest.api.keeptrackapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeepTrackApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
