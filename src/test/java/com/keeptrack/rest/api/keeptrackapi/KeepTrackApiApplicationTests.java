ce1cf3dfa76170a38d5f5c6998380fbe25319aa0
Vlad Vartolomei <vlad.vartolomei@gmail.com>
2017-05-25 21:47:47 +0300

    update dependencies, dry run 1

--- src/test/java/com/keeptrack/rest/api/keeptrackapi/KeepTrackApiApplicationTests.java
+++ src/test/java/com/keeptrack/rest/api/keeptrackapi/KeepTrackApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KeepTrackApiApplicationTests {
+class KeepTrackApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
